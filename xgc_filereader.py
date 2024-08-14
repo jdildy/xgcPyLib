ffefb3c9c2ca42f135ad748a464396a1037fd4ef
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-14 15:06:15 -0400

    Adding user functionality (select timestep)

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -23,62 +23,60 @@ class xgc1(object):
         self.vars = {}
         print("Reading XGC Output Data...")
         print("Getting Time Slice Data...")
-        self.time = self.find_timeslice()
+        self.time = self.xgc1_timeslice()
         print("TimeSlice Data Capture Complete.")
-
-        
         self.length = len(self.time)
 
         self.step = self.time[1] - self.time[0]
         self.start = self.time[0] 
         self.end = self.time[-1] + self.step
+    
         
 
-        istart = self.start
-        iend = self.end
-        istep = self.step
+        istart = self.start #2
+        iend = self.end #2172
+        istep = self.step #2
 
-        print(istart)
-        print(iend)
-        print(istep)
+        self.length = len(self.time)
+
+        print("File Info: \n")
+        print("First Timestep: " + istart)
+        print("Last Timestep: " + str(iend - istep))
+        print("Step: " + istep)
+
+
+
         
-        #XGC.3D.Reader
-        print("Reading xgc.3d.xxxx.bp files...")
-        for i in range(istart, iend, istep):
-            filename = xgc_path + "/xgc.3d.%5.5d.bp" %(i)
-            #print(filename)
-            try:
-                with Stream(filename,"rra") as f:
-                    self.dpot = f.read("dpot")
-                    self.dden = f.read('eden')
-            except Exception as e:
-                print(f"Error reading file: {e}")
-        print("xgc.3d.xxxxx.bp files read sucessfully.")
+        # #XGC.3D.Reader
+        # print("Reading xgc.3d.xxxx.bp files...")
+        # for i in range(istart, iend, istep):
+        #     filename = xgc_path + "/xgc.3d.%5.5d.bp" %(i)
+        #     #print(filename)
+        #     try:
+        #         with Stream(filename,"rra") as f:
+        #             self.dpot = f.read("dpot")
+        #             self.dden = f.read('eden')
+        #     except Exception as e:
+        #         print(f"Error reading file: {e}")
+        # print("xgc.3d.xxxxx.bp files read sucessfully.")
 
 
-        #XGC.F3D.Reader
-
-        print("Reading xgc.f3d.xxxx.bp files...")
-        for i in range(istart, iend, istep):
-            filename = xgc_path + "/xgc.f3d.%5.5d.bp" %(i)
-            #print(filename)
-            try:
-                with Stream(filename,"rra") as f:
-                    i_pol_n0_f0= f.read('i_poloidal_flow_n0_f0')
-                    e_pol_n0_f0= f.read('e_poloidal_flow_n0_f0')
-            except Exception as e:
-                print(f"Error reading file: {e}")
-        print("xgc.f3d.xxxxx.bp files read sucessfully.")
-
-
+        # #XGC.F3D.Reader
+        # print("Reading xgc.f3d.xxxx.bp files...")
+        # for i in range(istart, iend, istep):
+        #     filename = xgc_path + "/xgc.f3d.%5.5d.bp" %(i)
+        #     #print(filename)
+        #     try:
+        #         with Stream(filename,"rra") as f:
+        #             self.i_pol_n0_f0= f.read('i_poloidal_flow_n0_f0')
+        #             self.e_pol_n0_f0= f.read('e_poloidal_flow_n0_f0')
+        #     except Exception as e:
+        #         print(f"Error reading file: {e}")
+        # print("xgc.f3d.xxxxx.bp files read sucessfully.")
 
 
 
-
-
-
-
-    def reader(self, file):
+    def xgc1reader(self, file):
         try:
             with Stream(file, "rra") as f:
                 for _ in f.steps():
@@ -92,12 +90,8 @@ class xgc1(object):
         except Exception as e:
             print(f"Error reading file: {e}")
         return self.vars
-        
 
-
-
-
-    def find_timeslice(self):
+    def xgc1_timeslice(self):
             path = self.xgc_path
             bp_timeslices = []
 
@@ -124,19 +118,99 @@ class xgc1(object):
 
 
 
+# class xgca(object):
+#     def __init__(self,xgc_path):
+#         self.xgc_path = os.path.join(xgc_path,'')  #get file_path, add path separator if not there
+#         print(str(self.xgc_path))
+#         self.vars = {}
+#         print("Reading XGC Output Data...")
+#         print("Getting Time Slice Data...")
+#         self.time = self.xgca_timeslice()
+#         print("TimeSlice Data Capture Complete.")
 
+        
+#         self.length = len(self.time)
 
-
-
-
-            
-    
+#         self.step = self.time[1] - self.time[0]
+#         self.start = self.time[0] 
+#         self.end = self.time[-1] + self.step
         
 
-    # Call methods to demonstrate functionality
+#         istart = self.start #2
+#         iend = self.end #2172
+#         istep = self.step #2
+        
+#         #XGC.2D.Reader
+#         print("Reading xgc.2d.xxxx.bp files...")
+#         for i in range(istart, iend, istep):
+#             filename = xgc_path + "/xgc.2d.%5.5d.bp" %(i)
+#             #print(filename)
+#             try:
+#                 with Stream(filename,"rra") as f:
+                    
+#             except Exception as e:
+#                 print(f"Error reading file: {e}")
+#         print("xgc.2d.xxxxx.bp files read sucessfully.")
 
 
-fileDir = '/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun'
+#         #XGC.F2D.Reader
+
+#         print("Reading xgc.f2d.xxxx.bp files...")
+#         for i in range(istart, iend, istep):
+#             filename = xgc_path + "/xgc.f2d.%5.5d.bp" %(i)
+#             #print(filename)
+#             try:
+#                 with Stream(filename,"rra") as f:
+                    
+#             except Exception as e:
+#                 print(f"Error reading file: {e}")
+#         print("xgc.f3d.xxxxx.bp files read sucessfully.")
 
 
-loader=xgc1(fileDir)
+
+#     def xgcareader(self, file):
+#         try:
+#             with Stream(file, "rra") as f:
+#                 for _ in f.steps():
+#                     self.vars = f.available_variables()
+#                     # # if isinstance(vars, dict):
+#                     # #     for name, info in vars.items():
+#                     # #         print("variable_name: " + name, end=" ")
+#                     #         # for key, value in info.items():
+#                     #         #     #print("\t" + key + ": " + value, end=" ")
+#                     #     print()
+#         except Exception as e:
+#             print(f"Error reading file: {e}")
+#         return self.vars
+
+#     def xgca_timeslice(self):
+#             path = self.xgc_path
+#             bp_timeslices = []
+
+#             pattern = re.compile(r'\b2d\.(\d+)\.bp\b')
+
+#             if os.path.exists(path):
+#                 for root, dirs, files in os.walk(path):
+#                     for dir in dirs:
+#                         match = pattern.search(dir)
+#                         if match:
+#                             number = match.group(1)
+#                             #print(number)
+#                             bp_timeslices.append(number)
+#             else:
+#                 print(f"The 'rundir' directory does not exist at path: {path}\n")
+            
+#             print("Number of files found: " + str(len(bp_timeslices)))
+
+#             numbers = [int(item) for item in bp_timeslices]
+#             numbers.sort()
+#             # sorted_array = [f"{num:05d}" for num in numbers]
+#             # print(sorted_array) WORKS 
+#             return numbers
+
+
+
+# fileDir = '/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun'
+
+
+# loader=xgc1(fileDir)
