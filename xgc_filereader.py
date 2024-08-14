5b9d8fd66b9b2694176f44e2af00e123422aac30
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-14 09:37:23 -0400

    Generating TimeSlice info

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -8,48 +8,46 @@ from scipy.optimize import curve_fit
 from scipy.special import erfc
 import scipy.sparse as sp
 from tqdm.auto import trange, tqdm
-import os 
+
+
+import os
 import re
 
-
-#3D Diagnostic Reader
-class xgc1(object): 
+class xgc1(object):
     def __init__(self):
         self.path = "/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun" + '/'
 
-        def reader(self, file):
-            with Stream(file, "rra") as f:
-                for _ in f.steps():
-                    for name, info in f.available_variables().items():
-                        print("variable_name: " + name, end=" ")
-                        for key, value in info.items():
-                            print("\t" + key + ": " + value, end=" ")
-                        print()
-
-        def find_timeslice():
-            path = self.path
-
-            bp_timeslices = []
-
-            pattern = re.compile(r'\b(?:3d|f3d)\.(\d+)\.bp\b')
-
-            if os.path.exists(path):
-                 for root, dirs, files in os.walk(path):
-                    for dir in dirs: 
-                        match = pattern.search(dir)
-                        if match:
-                            full_path = os.path.join(dir)
-                            print(full_path)
-                            bp_timeslices.append(full_path)
-            else: 
-                print(f"The 'rundir' directory does not exist at path: {rundir}\n")
-            return bp_timeslices
-    
-
+        # Call methods to demonstrate functionality
         print("Reading XGC Output Data...")
         print("Getting Time Slice Data...")
-        self.time = find_timeslice()
+        #self.time = self.find_timeslice()
         print("TimeSlice Data Capture Complete.")
 
-       
+    def reader(self, file):
+        with Stream(file, "rra") as f:
+            for _ in f.steps():
+                for name, info in f.available_variables().items():
+                    print("variable_name: " + name, end=" ")
+                    for key, value in info.items():
+                        print("\t" + key + ": " + value, end=" ")
+                    print()
+
+    def find_timeslice(self):
+        path = self.path
+
+        bp_timeslices = []
+
+        pattern = re.compile(r'\b(?:3d|f3d)\.(\d+)\.bp\b')
+
+        if os.path.exists(path):
+            for root, dirs, files in os.walk(path):
+                for dir in dirs:
+                    match = pattern.search(dir)
+                    if match:
+                        full_path = os.path.join(root, dir)
+                        print(full_path)
+                        bp_timeslices.append(full_path)
+        else:
+            print(f"The 'rundir' directory does not exist at path: {path}\n")
         
+        return bp_timeslices
