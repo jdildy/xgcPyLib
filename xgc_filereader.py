1cec28605b291a17b5cefa74536e5b9034a26028
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-14 10:03:37 -0400

    Generating TimeSlice info

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -14,13 +14,16 @@ import os
 import re
 
 class xgc1(object):
-    def __init__(self):
-        self.path = "/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun" + '/'
+    def __init__(self,xgc_path):
+        self.xgc_path = os.path.join(xgc_path,'')  #get file_path, add path separator if not there
 
         print("Reading XGC Output Data...")
         print("Getting Time Slice Data...")
-        self.time = self.find_timeslice()
+        #self.time = self.find_timeslice()
         print("TimeSlice Data Capture Complete.")
+
+
+
 
     def reader(self, file):
             with Stream(file, "rra") as f:
@@ -55,3 +58,8 @@ class xgc1(object):
 
     # Call methods to demonstrate functionality
 
+
+fileDir = '/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun'
+
+
+loader=xgc1(fileDir)
