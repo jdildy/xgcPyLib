a29100ba394b44138bb898048c28973cd07ac859
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-15 16:09:30 -0400

    Mult-file read refactoring

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -129,15 +129,17 @@ class xgc1(object):
                     # #1D Arrays Not working for some reason... Will ask Robert
                     self.e_marker_den = f.read('e_marker_den')
                     self.i_marker_den = f.read('i_marker_den')
+                    #works
 
                     # self.i_marker_weight = f.read('i_marker_weight')
                     # self.e_marker_weight = f.read('e_marker_weight')
 
                     self.e_weight_variance = f.read('e_weight_variance')
                     self.i_weight_variance = f.read('i_weight_variance')
+                    #works
 
-                    # self.pot0 = f.read('pot0')
-                    # self.pot0m = f.read('pot0m')
+                    self.pot0 = f.read('pot0')
+                    self.pot0m = f.read('pot0m')
 
             
                     #Scalar #works
