7cfd78886c75248427436b3aeb0b7e946cab77b2
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-19 09:19:06 -0400

    Testing

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -954,7 +954,7 @@ class loader(object):
 fileDir = '/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun'
 
 #required
-#xgc1Obj = xgc1(fileDir)
+xgc1Obj = xgc1(fileDir)
 #xgcaObj = xgca(fileDir)
 #manager = loader(fileDir)
 
@@ -962,7 +962,7 @@ fileDir = '/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile_Jun'
 # i_radial_en_flux_ExB_turb_df = xgc1Obj.get_loadVar3D('i_radial_en_flux_ExB_turb_df')
 # print(i_radial_en_flux_ExB_turb_df)
 
-data1Obj = data1(fileDir)
+#data1Obj = data1(fileDir)
 
 
 
