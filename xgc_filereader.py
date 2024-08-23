5305629f119b79395c2fa2289ed5ee9bb448876a
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-23 15:46:47 -0400

    testing sheathdiag reader

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -608,6 +608,7 @@ class shealth(object):
                 if nsize != '':
                     var = r.inquire_variable(variable)
                     ndim = var.shape()
+                    ndim = len(ndim)
                     print(len(ndim))
                     if ndim ==1: 
                         print("1D Array")
