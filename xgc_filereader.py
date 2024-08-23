79fbba8a70bb15fecc1d13c4f70ec50eb1e32244
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-23 15:55:58 -0400

    testing sheathdiag reader

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -612,13 +612,16 @@ class shealth(object):
                     if len(ndim) == 1: 
                         nsize = int(nsize)
                         data = r.read(variable,start=[0], count=[nsize],  step_selection=[0, nstep])
+                        print("1D")
                     elif len(ndim) == 2: 
                         nsize = int(nsize)
                         data = r.read(variable,start=[0,0], count=[rows, columns],  step_selection=[0, nstep])
+                        print("2D")
                     else:
                         print("Error: Too many dimensions.")
-                else: #mostly xgc.oneddiag
+                else: #scalar
                     data = r.read(variable,start=[], count=[], step_selection=[0, nstep])
+                    print("scalar")
             return data
                 
                 # return data        
