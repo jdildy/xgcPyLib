4fb40bb4d27ef7b3709d45c4726a319d90cd49b8
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-25 19:20:37 -0400

    testing sheathdiag reader

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -604,7 +604,7 @@ class sheath(object):
             with Stream(self.xgc_path + '/xgc.sheathdiag.bp', 'rra') as r:
                 nstep = int(r.available_variables()[variable]['AvailableStepsCount'])
                 nsize = r.available_variables()[variable]['Shape']
-                
+                print(nsize)
                 if nsize != '':
                     var = r.inquire_variable(variable)
                     ndim = var.shape
