c7e9c23846e174415c54cffb588cc026feb57580
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-25 20:15:33 -0400

    testing sheathdiag reader

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -608,9 +608,9 @@ class sheath(object):
                 
                 cdim = str(nsize)
                 array_count = cdim.split(',')
-                # array_count = [int(number.strip()) for number in array_count]
+                array_count = [int(number.strip()) for number in array_count]
 
-                # print(array_count)
+                print(array_count)
                
                 # print(len(array_count))
                 # if nsize != '':
