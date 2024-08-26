5aa72f6fd098b67fa78a9014ea0534a2f96ce829
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-25 20:21:07 -0400

    testing sheathdiag reader

--- xgc_filereader.py
+++ xgc_filereader.py
@@ -604,40 +604,41 @@ class sheath(object):
             with Stream(self.xgc_path + '/xgc.sheathdiag.bp', 'rra') as r:
                 nstep = int(r.available_variables()[variable]['AvailableStepsCount'])
                 nsize = r.available_variables()[variable]['Shape']
-                print(nsize)
                 
-                cdim = str(nsize)
-                print(cdim)
-                array_count = cdim.split(',')
-                #array_count = [int(number.strip()) for number in array_count]
+            
+                if nsize != '':
+                    var = r.inquire_variable(variable)
+                    ndim = var.shape
 
-                # print(array_count)
-               
-                # print(len(array_count))
-                # if nsize != '':
-                #     var = r.inquire_variable(variable)
-                #     ndim = var.shape
+                    rows = var.shape()[0]
+                    columns = var.shape()[1]
+                    print(rows)
+                    print(columns)
 
-                #     rows = var.shape()[0]
-                #     columns = var.shape()[1]
-                #     print(rows)
-                #     print(columns)
+                    print(nsize)
+                
+                    cdim = str(nsize)
+                    print(cdim)
+                    array_count = cdim.split(',')
+                    array_count = [int(number.strip()) for number in array_count]
+
+                    print(array_count)
                     
 
-                #     if len(array_count) == 1: 
-                #         nsize = int(nsize)
-                #         data = r.read(variable,start=[0], count=[nsize],  step_selection=[0, nstep])
-                #         print("1D")
-                #     elif len(array_count) == 2: 
-                #         nsize = int(nsize)
-                #         data = r.read(variable,start=[0,0], count=[rows, columns],  step_selection=[0, nstep])
-                #         print("2D")
-                #     else:
-                #         print("Error: Too many dimensions.")
-                # else: #scalar
-                #    data = r.read(variable,start=[], count=[], step_selection=[0, nstep])
-                #    print("scalar")
-                #return data
+                    if len(ndim) == 1: 
+                        nsize = int(nsize)
+                        data = r.read(variable,start=[0], count=[nsize],  step_selection=[0, nstep])
+                        print("1D")
+                    elif len(ndim) == 2: 
+                        nsize = int(nsize)
+                        data = r.read(variable,start=[0,0], count=[rows, columns],  step_selection=[0, nstep])
+                        print("2D")
+                    else:
+                        print("Error: Too many dimensions.")
+                else: #scalar
+                   data = r.read(variable,start=[], count=[], step_selection=[0, nstep])
+                   print("scalar")
+                return data
                 
                 # return data        
         except Exception as e:
