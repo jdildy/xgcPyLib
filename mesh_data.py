3f505645432a32f91a683f71dbd8aa1e6a6ac4c5
jdildy18 <97419424+jdildy18@users.noreply.github.com>
2024-08-28 20:47:12 -0400

    Analyzing oneddiag

--- mesh_data.py
+++ mesh_data.py
@@ -37,9 +37,8 @@ fileDir = xgc_filereader.checkpath('/pscratch/sd/s/sku/n552pe_d3d_NT_new_profile
 
 xgc1Obj = xgc_filereader.xgc1(fileDir)
 
-
+xgc1Obj.xgc1_reader('/xgc.3d.00098.bp')
 dpot = xgc1Obj.get_loadVar3D('dpot')
-print(len(dpot[0][0]))
 # dpot = 
 # data3D = xgc1Obj.xgc1_readmult3D('dpot',2,6,2)
 # print(len(data3D))
